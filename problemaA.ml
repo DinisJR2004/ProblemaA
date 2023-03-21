1c7dad8dff38e9e4b425b148fdee5144f8e5817a
Dinis Ramos <119667762+DinisJR2004@users.noreply.github.com>
2023-03-21 16:45:11 +0000

    Update problemaA.ml

--- problemaA.ml
+++ problemaA.ml
@@ -50,3 +50,42 @@ else
 print_int (motz n)
 
 in()
+
+
+--------------------------------------------------------------------------------------------------------
+
+(****************************************************************************************************)
+(*                                                                                                  *)
+(*                                        Programação Funcional                                     *)
+(*                                             Problema A                                           *)
+(*                                                                                                  *)
+(*                                                                                                  *)
+(*     Realizado por:   Dinis Ramos     - nº 49471                                                  *)
+(*                      Ricardo Andrade - nº 49653                                                  *)
+(*                                                                                                  *)
+(****************************************************************************************************)
+
+(* Função que imprime na tela a frase "Digite número de 0 a 10000:" *)
+print_string "Digite número de 0 a 10000:\n"; 
+
+(* Leitura do número *)
+let n = read_int () in
+
+(* Verifica se o número está dentro do intervalo válido *)
+if n < 0 || n > 10000 then 
+  (* Número inválido *)
+  print_endline "Número inválido"
+else
+  (* Cria um vetor de tamanho n+1 e inicializa com zeros *)
+  let motzkin_numbers = Array.make (n+1) 0 in
+
+  (* Calcula os números de Motzkin e armazena no vetor *)
+  for i = 0 to n do
+    motzkin_numbers.(i) <- 
+      if i < 2 then 1
+      else ((2*i + 1) * motzkin_numbers.(i-1) + (3*i - 3) * motzkin_numbers.(i-2)) / (i + 2)
+  done;
+
+  (* Imprime o resultado *)
+  print_int motzkin_numbers.(n);
+  print_newline();  (* Quebra de linha para melhorar a legibilidade *)
