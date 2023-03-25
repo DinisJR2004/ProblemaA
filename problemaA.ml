8601db54a2a126a73b393a4ae1c84f9e53562e1a
Dinis Ramos <119667762+DinisJR2004@users.noreply.github.com>
2023-03-25 18:35:11 +0000

    Update problemaA.ml

--- problemaA.ml
+++ problemaA.ml
@@ -54,38 +54,31 @@ in()
 
 --------------------------------------------------------------------------------------------------------
 
-(****************************************************************************************************)
-(*                                                                                                  *)
-(*                                        Programação Funcional                                     *)
-(*                                             Problema A                                           *)
-(*                                                                                                  *)
-(*                                                                                                  *)
-(*     Realizado por:   Dinis Ramos     - nº 49471                                                  *)
-(*                      Ricardo Andrade - nº 49653                                                  *)
-(*                                                                                                  *)
-(****************************************************************************************************)
+let () = print_string "Digite número de 0 a 10000:\n" in
 
-(* Função que imprime na tela a frase "Digite número de 0 a 10000:" *)
-print_string "Digite número de 0 a 10000:\n"; 
-
-(* Leitura do número *)
 let n = read_int () in
 
-(* Verifica se o número está dentro do intervalo válido *)
-if n < 0 || n > 10000 then 
-  (* Número inválido *)
-  print_endline "Número inválido"
+if n < 0 || n > 10000 then print_endline "Número inválido"
+
 else
-  (* Cria um vetor de tamanho n+1 e inicializa com zeros *)
-  let motzkin_numbers = Array.make (n+1) 0 in
+  let motz_array = Array.make (n+1) 0 in
 
-  (* Calcula os números de Motzkin e armazena no vetor *)
-  for i = 0 to n do
-    motzkin_numbers.(i) <- 
-      if i < 2 then 1
-      else ((2*i + 1) * motzkin_numbers.(i-1) + (3*i - 3) * motzkin_numbers.(i-2)) / (i + 2)
-  done;
+  let rec motz n =
+    if n < 2 then
+      begin
+        motz_array.(n) <- 1;
+        1
+      end
+    else
+      begin
+        if motz_array.(n) = 0 then
+          motz_array.(n) <- ((2*n + 1) * motz(n-1) + (3*n - 3) * motz(n-2)) / (n+2);
+        motz_array.(n)
+      end
+  in
 
-  (* Imprime o resultado *)
-  print_int motzkin_numbers.(n);
-  print_newline();  (* Quebra de linha para melhorar a legibilidade *)
+  let result = motz n in
+  print_int result;
+  print_newline ();
+  Array.iter print_int motz_array
+
