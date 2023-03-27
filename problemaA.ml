8c01bc07eb0b0fdf49563267ad3c63847f079657
Dinis Ramos <119667762+DinisJR2004@users.noreply.github.com>
2023-03-27 20:35:43 +0100

    Update problemaA.ml

--- problemaA.ml
+++ problemaA.ml
@@ -9,76 +9,61 @@
 (*                                                                                                  *)
 (****************************************************************************************************)
 
-(* 
 
-  Função que imprime na tela a frase "Digite número de 0 a 10000:" ;
+(*
+
+  É defino uma variável "n" que usa a função read_int() para ler um número inteiro fornecido pelo usuário.
+
+*)
+let n = read_int ()
+
+
+(*
    
+  Função cria um array chamado "v" com 10001 elementos, cada um inicializado com o valor inteiro zero.
+
 *)
 
-let () = print_string "Digite número de 0 a 10000:\n" in
+let v = Array.make 10001 (Z.of_int 0)
 
-
-(* 
-
-  Leitura do número ;
+(*
    
-*)
-
-let n = read_int () in
-
-(* 
- 
-  Função que faz a verificação do valor inserido ; 
-  Se o número estiver fora do intervalo é impresso no ecrã "Número inválido" ;
-  Senão passa para o calculo do número de Motzkin ;
-   
-*)
-
-let () =
-
-if n < 0 || n > 10000 then print_endline "Número inválido"
-
-else
+  É defina uma variável "num" que armazenará o valor final da sequência de Motzkin calculada para o número fornecido pelo usuário.
+  Na linha 39 temos uma condicional que verifica se o número fornecido está dentro do intervalo válido de 0 a 10000, se não estiveres é devolvido o número 0.
+  Na linha 40 é defina uma função recursiva chamada "motz" que calcula a sequência de Motzkin.
+  Na linha 42 temos uma condicional que verifica se o número já foi calculado antes e armazenado no array "v". 
+  Se já foi calculado, o valor armazenado no array é retornado imediatamente, evitando a necessidade de calcular novamente.
+  Na linha 45 é armazeno o valor calculado no array "v" para uso posterior.
   
-  let rec motz n =
-    
-    if n < 2 then 1
-    
-    else ((2*n + 1) * motz(n - 1) + (3*n - 3) * motz(n - 2)) / (n + 2)
-  in
-
-print_int (motz n)
-
-in()
 
 
---------------------------------------------------------------------------------------------------------
+*)
 
-let () = print_string "Digite número de 0 a 10000:\n" in
+let num =
+  if n >= 0 && n <= 10000 then
+    let rec motz n =
+      if n < (Z.of_int 2) then Z.of_int 1
+      else 
+        if (v.(Z.to_int n) != (Z.of_int 0)) then v.(Z.to_int n)
+        else
+          let result = Z.div
+            (Z.add (Z.mul (Z.add (Z.mul (Z.of_int 2) n) (Z.of_int 1))
+                  (motz (Z.sub n (Z.of_int 1))))
+                  (Z.mul (Z.sub (Z.mul (Z.of_int 3) n) (Z.of_int 3))
+                  (motz (Z.sub n (Z.of_int 2)))))
+            (Z.add n (Z.of_int 2))
+          in
+          v.(Z.to_int n) <- result;
+          result
+    in
+    motz (Z.of_int n)
+  else 
+    (Z.of_int 0)
 
-let n = read_int () in
 
-if n < 0 || n > 10000 then print_endline "Número inválido"
+(*
+   
+  Função para imprimir o valor final da sequência de Motzkin calculado.
 
-else
-  let motz_array = Array.make (n+1) 0 in
-
-  let rec motz n =
-    if n < 2 then
-      begin
-        motz_array.(n) <- 1;
-        1
-      end
-    else
-      begin
-        if motz_array.(n) = 0 then
-          motz_array.(n) <- ((2*n + 1) * motz(n-1) + (3*n - 3) * motz(n-2)) / (n+2);
-        motz_array.(n)
-      end
-  in
-
-  let result = motz n in
-  print_int result;
-  print_newline ();
-  Array.iter print_int motz_array
-
+*)
+let () = print_endline (Z.to_string num)
